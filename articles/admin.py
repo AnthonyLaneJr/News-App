6a564002d8b5e776b5be3656e61484409497a799
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-07-31 11:14:15 -0700

    Add files via upload

---
+++ articles/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Section, ArticleType, Article, Status
+
+
+admin.site.register([Section, ArticleType, Article, Status])
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://whispering-reaches-35009.herokuapp.com/
