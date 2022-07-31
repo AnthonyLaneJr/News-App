2dbd4842f7cb23e999faa9589860bd2bbebb827f
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-07-31 16:31:36 -0700

    Add files via upload

--- articles/urls.py
+++ articles/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/delete', ArticleDeleteView.as_view(), name='article_delete'),
-    path('', ArticleTotalListView.as_view(), name='article_list'),
+    path('', ArticleTotalListView.as_view(), name='article_total_list'),
 ]
